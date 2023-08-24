2acbe704a6d3129923a4d848bc877cc7a3186bdc
AbrarSaud <abrar.sauod0@gmail.com>
2023-08-24 13:26:33 +0300

    add :WelcomePage and SignInePage

---
+++ lib/constants/spacings.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const hVSpace4 = SizedBox(height: 4);
+const hVSpace8 = SizedBox(height: 8);
+const hVSpace16 = SizedBox(height: 16);
+const hVSpace24 = SizedBox(height: 24);
+const hVSpace32 = SizedBox(height: 32);
+const hVSpace64 = SizedBox(height: 64);
+const hVSpace128 = SizedBox(height: 128);
+
+const hHSpace4 = SizedBox(width: 4);
+const hHSpace8 = SizedBox(width: 8);
+const hHSpace16 = SizedBox(width: 16);
+const hHSpace24 = SizedBox(width: 24);
+const hHSpace32 = SizedBox(width: 32);
+const hHSpace64 = SizedBox(width: 64);
+const hHSpace128 = SizedBox(width: 128);
